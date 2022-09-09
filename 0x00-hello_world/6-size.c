dcc1178a6c4e87405924dc4a17112b123f8c4228
Emebetyw <Emuyoha21@gmail.com>
2022-09-09 10:33:40 +0300

    display all data type size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,7 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
+
 	printf("Size of a char: %d byte(s)\n", sizeof(a));
 	printf("Size of an int: %d byte(s)\n", sizeof(b));
 	printf("Size of a long int: %d byte(s)\n", sizeof(c));
