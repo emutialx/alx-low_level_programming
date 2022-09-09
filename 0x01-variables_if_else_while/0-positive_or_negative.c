7c0d9ea280b3320b76747c81270343d6466979bb
Emebetyw <Emuyoha21@gmail.com>
2022-09-09 14:48:22 +0300

    check if the number is 0 or +ve or -ve

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,16 +14,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n>0)
+	if (n > 0)
 	{
-		printf("%d is positive",n);
+		printf("%d is positive", n);
 	}
-	else if (n<0)
+	else if (n == 0)
 	{
-		printf("%d is negative",n);
+		printf("%d is zero", n);
+	}
+	else
+	{	printf("%d is negative", n);
 	}
 	return (0);
-	else
-	{	printf("%d is zero",n);
-	}
 }
