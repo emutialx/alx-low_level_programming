7c112d5c1db90d0973dd7d99dad4447d3fc06285
Emebetyw <Emuyoha21@gmail.com>
2022-09-11 18:30:18 +0300

    a program which prints all possible combination numbers of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -20,8 +20,8 @@ int main(void)
 				if (digit1 == 8 && digit2 == 9)
 					
 					continue;
-																										putchar(',');
-																										putchar(' ');
+					putchar(',');
+					putchar(' ');
 			}
 		}
 
