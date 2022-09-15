f5ae67b4b9d6290826c6ec6f28d6bd199185fa92
Emebetyw <Emuyoha21@gmail.com>
2022-09-15 16:54:33 +0300

    print 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Multiplies two integers
+ *
+ * @y: The number to pe printed
+ *
+ * Return: The result of multiplication between x param and y param
+ */
+void print_numbers(void)
+{
+	int y;
+	for (y ='0'; y <= '9'; y++)
+		putchar(y);
+	putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,3 +9,4 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int, int);
+void print_numbers(void)
