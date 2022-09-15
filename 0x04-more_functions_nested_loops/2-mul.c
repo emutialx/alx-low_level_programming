d25190a186f14c13d1573242781b62f8e281e516
Emebetyw <Emuyoha21@gmail.com>
2022-09-15 16:38:33 +0300

    multiply two nubers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _isupper - Check if a letter is upper
+ * @a: The number to be multiplied
+ * @b: The number to be multiplied
+ *
+ * Return: 1 for upper letter or 0 for any else
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,3 +8,4 @@
 #include <stdio.h>
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int, int);
