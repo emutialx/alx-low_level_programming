5dd905224e1cd31f2fb3f6801c0ad791e0a968f8
Emebetyw <Emuyoha21@gmail.com>
2022-09-15 17:37:12 +0300

    print 0 to 9 except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_most_numbers - entry point
  *
- * Description: Prints the numbers with _putchar
+ * @c: numbers to be printed
  *
  * Return: void
  */
@@ -12,8 +12,11 @@ void print_most_numbers(void)
 	int c;
 
 	for (c = 48; c <= 57; c++)
+	{
 		if (!(c == 50) && !(c ==52))
+		{
 			_putchar(c);
+		}
+	}
 	_putchar('\n');
-	
 }
