b4d755db645d0985daf28c55a401da273b5362b8
Emebetyw <Emuyoha21@gmail.com>
2022-09-16 05:53:49 +0300

    primefactors

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 
 /**
- *  * main - prints the highest prime factor of 612852475143
- *   *
- *    * Return: 0 if successful
- *     */
+ * main - prints the highest prime factor of 612852475143
+ *
+ * Return: 0 if successful
+ */
 int main(void)
 {
-		unsigned long int prime = 612852475143;
-			unsigned long int i = 3;
 
-				while (i <= prime / 2)
-							if (prime % i == 0)
-											prime = prime / i;
-						else
-										i += 2;
+	unsigned long int prime = 612852475143;
+	unsigned long int i = 3;
 
-					printf("%ld\n", prime);
-
-						return (0);
+	while (i <= prime / 2)
+		if (prime % i == 0)
+			prime = prime / i;
+		else
+			i += 2;
+	printf("%ld\n", prime);
+	return (0);
 }
