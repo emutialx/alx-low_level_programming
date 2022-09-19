3f5d6dc618a160f67a96d37596a1aee3af3a72eb
Emebetyw <Emuyoha21@gmail.com>
2022-09-19 17:03:05 +0300

    lenght of a string fuction added

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - check the code
+ *@a: - pointer
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	while (*s != '\0')
+	{	s++;
+		l++;
+	}
+	return (1);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,4 +5,5 @@
 void reset_to_98(int *n);
 int _putchar(char);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 #endif
