cc89db0fb3906971d27e0f515e874e2980d8c493
Emebetyw <Emuyoha21@gmail.com>
2022-09-26 21:26:18 +0300

    added readme andfunctions memory with constant byte

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+void set_string(char **s, char *to);
+  
+#endif
