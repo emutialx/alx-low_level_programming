07436e199ea4e1af0e78301193538a9f452bd108
emutialx <emutialx@gmail.com>
2022-10-06 16:31:18 -0700

    advanced task

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - Allocates space of `new_size` 
+ * bytes and moves old data to new space.
+ * @ptr: Pointer to the old memory space
+ * @old_size: The size in bytes of the old memory space
+ * @new_size: The size in bytes of the new memory space
+ *
+ * Return: Pointer to the new memory space
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new;
+	unsigned int i;
+
+	if (ptr == NULL)
+		return (malloc(new_size));
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	new = malloc(new_size);
+	if (new == NULL)
+		return (NULL);
+	for (i = 0; i < old_size && i < new_size; i++)
+		*((unsigned char *)new + i) = *((unsigned char *)ptr + i);
+	return (new);
+}
