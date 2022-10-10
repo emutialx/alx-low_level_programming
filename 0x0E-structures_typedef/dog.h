79611324f17a285e517bf4e91e82aebb1b696cf4
Emebetyw <Emuyoha21@gmail.com>
2022-10-10 21:05:08 +0300

    added a description on dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,12 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
-/*
+/**
  * dog_t - Typedef for struct dog
  */
 typedef struct dog dog_t;
 
-/*
+/**
  * struct dog - struct that stores some information of a dog
  * @name: name of the dog
  * @age: age of the dog
@@ -21,8 +21,10 @@ struct dog
 	float age;
 	char *owner;
 };
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
