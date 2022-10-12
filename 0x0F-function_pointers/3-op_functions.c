e680bb9ee3c7aae7e72b8d3ae60a72cb88f0e760
Emebetyw <Emuyoha21@gmail.com>
2022-10-12 14:21:41 +0300

    all in one

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -4,7 +4,7 @@
  * op_add - Returns the sum of two numbers
  * @a:the first number
  * @b:the second number
- * 
+ *
  * Return:The sum of a and b
  */
 
@@ -13,12 +13,11 @@ int op_add(int a, int b)
 	return (a + b);
 }
 
-
 /**
  * op_sub - Returns the substraction of two numbers
  * @a:The first number
  * @b:The second number
- * 
+ *
  * Return: The Difference of a and b
  */
 int op_sub(int a, int b)
@@ -30,7 +29,7 @@ int op_sub(int a, int b)
  * op_mul - Returns the multiplication of two numbers
  * @a:The first number
  * @b: The second number
- * 
+ *
  * Return: The multiplication of a and b
  */
 int op_mul(int a, int b)
@@ -41,7 +40,7 @@ int op_mul(int a, int b)
  * op_div - Returns the division of two numbers.
  * @a: The first number.
  * @b: The second number.
- * 
+ *
  * Return: The quotient of a and b.
  */
 int op_div(int a, int b)
@@ -53,7 +52,7 @@ int op_div(int a, int b)
  * op_mod - Returns the remainder of the division of two numbers.
  * @a: The first number.
  * @b: The second number.
- * 
+ *
  * Return: The remainder of the division of a by b.
  */
 int op_mod(int a, int b)
