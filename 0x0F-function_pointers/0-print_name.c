2d19060e0de2cd3b605114e290654c51e9c3eabf
Emebetyw <Emuyoha21@gmail.com>
2022-10-12 13:42:47 +0300

    a function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * print_name - apointer function that prints name
+ * @name: name to print
+ * @f: function which print name
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if(name == NULL || f == NULL)
+		return;
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,4 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+void print_name(char *name, void (*f)(char *));
+#endif
