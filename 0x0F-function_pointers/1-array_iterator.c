e7dfa2ffa2c747f6ef11c61a19f86ad13993eca9
Emebetyw <Emuyoha21@gmail.com>
2022-10-12 13:58:23 +0300

    pointer function as parameter of array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,15 +5,12 @@
  * @array: array to be targeted
  * @size: size of array
  * @action: function to be executed
- * 
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-
 	if (array == NULL || action == NULL)
 		return;
-
 	while (size-- > 0)
 	{
 		action(*array);
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#include <stdlib.h>
+#include <stdio.h>
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
