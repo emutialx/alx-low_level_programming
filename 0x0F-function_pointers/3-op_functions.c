952953649e2f5f3fe8f60f948cca18cc1d7c0ec1
Emebetyw <Emuyoha21@gmail.com>
2022-10-12 14:19:04 +0300

    all in one

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -37,3 +37,26 @@ int op_mul(int a, int b)
 {
 	return (a * b);
 }
+/**
+ * op_div - Returns the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ * 
+ * Return: The quotient of a and b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Returns the remainder of the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ * 
+ * Return: The remainder of the division of a by b.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
