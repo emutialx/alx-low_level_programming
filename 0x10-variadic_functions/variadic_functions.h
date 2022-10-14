e7054aa6766a6d7abd0c12bd0bcb94bafd9694e4
Emebetyw <Emuyoha21@gmail.com>
2022-10-14 11:31:37 +0300

    fixed

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,4 +6,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
-#endif;
+#endif
