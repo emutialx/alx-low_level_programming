aca3ea048f170ebd92b08cb25426c9a94774d086
Emebetyw <Emuyoha21@gmail.com>
2022-12-11 19:12:18 +0300

    free all nodes

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,18 +1,30 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * free_listint2 - a function that frees the node starting from the first node
- * @head: head of the list
- * Return: void
- */
+  * free_listint2 - ...
+  * @head: ...
+  *
+  * Return: ...
+  */
 void free_listint2(listint_t **head)
 {
-listint_t *tmp;
+	listint_t *temp;
 
-while (*head)
-{
-tmp = (*head)->next;
-free(*head);
-(*head) = tmp->next;
-}
+	if (head)
+	{
+		while (*head)
+		{
+			temp = (*head);
+			*head = (*head)->next;
+			free(temp);
+		}
+	}
+	else
+	{
+		return;
+	}
+
+	free(*head);
+	head = 0;
 }
